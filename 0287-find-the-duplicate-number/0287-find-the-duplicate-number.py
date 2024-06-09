99e810c57672844bb46ff9591b79d7179b89d305
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-09 14:15:18 +0530

    Time: 754 ms (5.04%), Space: 30.4 MB (49.94%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,20 @@
+#Cycle Sort
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        i = 0
+        n = len(nums)
+
+        while (i < n):
+            corrected_index = nums[i] - 1 #'-1' because array range is start from '1'.
+
+            if nums[i] != nums[corrected_index]:
+                nums[i], nums[corrected_index] = nums[corrected_index], nums[i]
+
+            else: 
+                i += 1
+
+        #Traverse the indices
+        for i in range(n):
+            if (i + 1) != nums[i]:
+                return nums[i]
