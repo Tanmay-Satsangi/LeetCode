e3ee77db1303029ea298be3c26943a2d3226f66b
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-09 12:37:32 +0530

    Time: 505 ms (5.13%), Space: 24.8 MB (81.4%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,23 +1,27 @@
+#Cycle Sort
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        ans_list = []
         i = 0
 
-        ans_list = []
-
-        while (i < n):
+        while(i < n):
             corrected_index = nums[i] - 1
 
+            #Not Required to check for arr[i] < n becuase range is start from 1 So, 
+            #index and value at arr[i] is always less than or equal to last index of array.
+            
+            #Also, for this, we already decrement the corrected_index by 1 
             if nums[i] != nums[corrected_index]:
                 nums[i], nums[corrected_index] = nums[corrected_index], nums[i]
 
             else: 
                 i += 1
 
-        
         #Traverse through index
         for i in range(n):
-            if (i + 1) != nums[i]:
+            if nums[i] != (i + 1):
                 ans_list.append(i + 1)
 
         return ans_list
