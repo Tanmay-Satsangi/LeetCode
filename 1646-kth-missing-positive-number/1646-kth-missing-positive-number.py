59c811e0a244cbe34177c231b2e9fdfde5ae8b01
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-10 11:58:08 +0530

    Time: 52 ms (52.78%), Space: 16.7 MB (74.79%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.py
@@ -1,15 +1,23 @@
+#Binary Search
+
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        count = 0
         n = len(arr)
-        i = 1
 
-        while(True):
-            if i not in arr:
-                count += 1
-                if count == k:
-                    return i
-            i += 1
+        low = 0
+        high = (n - 1)
 
-        # return (i - 1)
-        
+        while(low <= high):
+            mid = (low + high) // 2
+
+#Add 1 because range is start from 1 so, at index 0, value 1 is present
+# Similarly, at index 1 value 2 is present.
+
+            missing = arr[mid] - (mid + 1) 
+
+            if missing >= k:  #i.e. if kth missing number is present in the range (low, mid)
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return high + k + 1
