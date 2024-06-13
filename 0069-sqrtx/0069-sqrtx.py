3062302105b759130227948646751361e1ee10d8
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-13 13:05:35 +0530

    Time: 39 ms (61.64%), Space: 16.5 MB (31.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,18 +1,21 @@
+#(0 ** 0) = 1 
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-
+        #If x == 0, then we have to return 0 because 0 ** 0 is 1. then it does not works for 0.
         if (x == 0):
             return 0
 
         low = 1
         high = x
 
-        while(low <= high):
+        while (low <= high):
             mid = (low + high) // 2
 
-            if (mid * mid <= x):
+            if (mid * mid) <= x:
                 ans = mid
-                low = mid + 1
+                low = mid + 1 #check for greater positive numer
             else:
                 high = mid - 1
 
