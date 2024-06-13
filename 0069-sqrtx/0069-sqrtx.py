86be81d9bb21ffae72efd7aaebb9e1831cabc574
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-13 13:07:59 +0530

    Time: 30 ms (94.98%), Space: 16.4 MB (96.87%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,14 +1,9 @@
 #(0 ** 0) = 1 
-
-
 class Solution:
     def mySqrt(self, x: int) -> int:
-        #If x == 0, then we have to return 0 because 0 ** 0 is 1. then it does not works for 0.
-        if (x == 0):
-            return 0
-
         low = 1
         high = x
+        ans = 0 #because if x == 0 then return 0. Also for 0 while loop does not run
 
         while (low <= high):
             mid = (low + high) // 2
