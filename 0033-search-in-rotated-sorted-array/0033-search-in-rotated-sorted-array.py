c4b65073ea4c95931f3845499ce9ab0041d54ed7
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-13 17:25:07 +0530

    Time: 45 ms (45.25%), Space: 16.8 MB (84.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,14 +1,10 @@
-# Binary Search 
-# TC = O(logN)
-# SC = O(1)
-
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
 
+        n = len(nums)
+        
         low = 0
-        high = n - 1
+        high = (n - 1)
 
         while(low <= high):
             mid = (low + high) // 2
@@ -16,18 +12,19 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
-            #Check left part is sorted 
-            if (nums[low] <= nums[mid]):
-                #Check condition for target and move low and high pointer
-                if (nums[low] <= target and target < nums[mid]):
+            #Check 1st part is sorted
+            if nums[low] <= nums[mid]:
+                #check target is present is 1st part
+                if nums[low] <= target and target < nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
 
+            #If 1st part is not sorted then definetely 2nd part is sorted.
             else:
                 if nums[mid] < target and target <= nums[high]:
                     low = mid + 1
-                else: 
+                else:
                     high = mid - 1
 
         return -1
