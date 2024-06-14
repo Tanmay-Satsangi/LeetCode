a223aaa874d7883df0df83305863a5f1c1921966
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-14 11:18:59 +0530

    Time: 37 ms (88.29%), Space: 17 MB (17.51%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -5,25 +5,43 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
-
         low = 0
-        high = (n - 1)
+        high = n - 1
 
-        if nums[low] <= nums[high]:
-            return nums[0]
-
-        while (low <= high):
+        while(low < high):
             mid = (low + high) // 2
 
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-
-            #Always minimum element is present in un-sorted part of array.
-            elif (nums[low] <= nums[mid]): #condition to check the part is sorted
+            if nums[mid] > nums[high]:
                 low = mid + 1
-        
+
             else:
-                high = mid - 1
+                high = mid
+
+
+        return nums[low]
+
+
+
+        # n = len(nums)
+
+        # low = 0
+        # high = (n - 1)
+
+        # if nums[low] < nums[high]:
+        #     return nums[0]
+
+        # while (low <= high):
+        #     mid = (low + high) // 2
+
+        #     if nums[mid] > nums[mid + 1]:
+        #         return nums[mid + 1]
+
+        #     #Always minimum element is present in un-sorted part of array.
+        #     elif (nums[low] <= nums[mid]): #condition to check the part is sorted
+        #         low = mid + 1
+        
+        #     else:
+        #         high = mid - 1
 
         
-        return -1
+        # return -1
