b4a71f6fb53d454218b7e823761027ab2e71074f
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-15 17:06:12 +0530

    Time: 33 ms (66.8%), Space: 16.5 MB (85.33%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -1,11 +1,12 @@
 # https://www.youtube.com/watch?v=2SVKgAakQsU
-# think to solve this problem just like to convert one number system to another number system (Ex: Decimal Number system to Binary Number System)
+# think to solve this problem just like to convert "one number system" to "another number system" (Ex: Decimal Number system to Binary Number System)
 
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         ans = []
 
         while (columnNumber):
+            # " -1 " because if the base is 26 then the number is lie from "0 to 25" but in the question the number is lie from "1 to 26" so, I decrease "-1" from each digit
             columnNumber -=1 
             rem = columnNumber % 26
             columnNumber = columnNumber // 26
