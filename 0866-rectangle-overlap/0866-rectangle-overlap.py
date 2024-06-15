a4e5b4e3457c527500dd52ed65e81dfcd078f378
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-15 17:10:44 +0530

    Time: 26 ms (95.32%), Space: 16.7 MB (6%) - LeetHub

--- 0866-rectangle-overlap/0866-rectangle-overlap.py
+++ 0866-rectangle-overlap/0866-rectangle-overlap.py
@@ -1,3 +1,19 @@
+# https://www.youtube.com/watch?v=UUjeqglXaSQ
+# lee215 Solution from discuss section
+
+# Intuition:
+# Before we do it in 2D plane, let's try it in 1D.
+# Given 2 segment (left1, right1), (left2, right2), how can we check whether they overlap?
+# If these two intervals overlap, it should exist an number x,
+
+# left1 < x < right1 && left2 < x < right2
+
+# left1 < x < right2 && left2 < x < right1
+
+# left1 < right2 && left2 < right1
+
+# This is the sufficient and necessary condition for two segments overlap.
+
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
         return (
