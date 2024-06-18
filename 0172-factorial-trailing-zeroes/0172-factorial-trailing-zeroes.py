e0fa5ca2fc0b9e7c530317cc0c06bc54c5df2fa1
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-18 14:12:14 +0530

    Time: 37 ms (63.48%), Space: 16.4 MB (76.32%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,3 +1,6 @@
+# TC = O(logN)
+# SC = O(1)
+
 # Approach: The two numbers which are responsible to make trailing zeroes are ' 5 & even number(2, 4, 6, 8)'. 
 #             Ex: 5! => 5 * 4 * 3 * 2 * 1 => 120 (Here 5 and 2 are responsible to make '0'). 
 #             Ex: 10! => 3628800 (Here 2 zeroes are come due to the 10(5 * 2) and second one 5 * 2)
