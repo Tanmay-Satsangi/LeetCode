5696199cdbd9ff63c5b36188824ec0a2bb6ae29d
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-18 14:06:22 +0530

    Time: 34 ms (78.1%), Space: 16.4 MB (76.32%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,17 @@
+# Approach: The two numbers which are responsible to make trailing zeroes are ' 5 & even number(2, 4, 6, 8)'. 
+#             Ex: 5! => 5 * 4 * 3 * 2 * 1 => 120 (Here 5 and 2 are responsible to make '0'). 
+#             Ex: 10! => 3628800 (Here 2 zeroes are come due to the 10(5 * 2) and second one 5 * 2)
+
+# But the count of 5 is always less then the even numbers so, we count the divisible of 5 in given variable 'n'
+
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+
+        while (n != 0):
+            n = n // 5
+            count += n
+
+        return count
+
