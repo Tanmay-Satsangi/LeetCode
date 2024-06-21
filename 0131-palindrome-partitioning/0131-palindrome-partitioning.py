7ce18d263145e94b1ed1554136b122d6ea8b625c
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-21 15:48:34 +0530

    Time: 449 ms (88.84%), Space: 30.1 MB (99.17%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -6,22 +6,22 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         part = []
-        
-        def dfs(i):
-            if i >= len(s):
-                res.append(part.copy())     #Because 'res' and 'curr' reference to the same array so, and in later code we have to apply operation on curr but not on res so, we have to create DEEP COPY. 
-
-                return 
-            
-            for j in range(i, len(s)):
-                #If string index 'i' to 'j' (Both Include) is Palindrome then, we append in list 'part' and after we call 'j + 1'
-                if self.isPalindrome(s, i, j):
-                    part.append(s[i: j + 1])
-                    dfs(j + 1)
-                    part.pop()      #Delete recently add(string), just before 2 lines.
-        
-        dfs(0)
+        self.backtrack(0, part, res, s)
         return res
+        
+    def backtrack(self, start, part, res, s):
+        if start >= len(s):
+            res.append(part.copy())     #Because 'res' and 'curr' reference to the same array so, and in later code we have to apply operation on curr but not on res so, we have to create DEEP COPY. 
+            return 
+        
+        for i in range(start, len(s)):
+            #If string index 'i' to 'j' (Both Include) is Palindrome then, we append in list 'part' and after we call 'j + 1'
+            if self.isPalindrome(s, start, i):
+                part.append(s[start: i + 1])
+                self.backtrack(i + 1, part, res, s)
+                part.pop()      #Delete recently add(string), just before 2 lines.
+    
+
                     
         #Code to check the given length is Palindrom or not(l and r both are included)    
     def isPalindrome(self, s, l, r):
@@ -33,3 +33,6 @@ class Solution:
             r -= 1
             
         return True
+                    
+            
+    
