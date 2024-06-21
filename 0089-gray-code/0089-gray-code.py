f05bdaeea3dfbd4ba609bed2993ce552e38c8be1
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-21 18:26:39 +0530

    Time: 89 ms (18.34%), Space: 25.2 MB (12.78%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,13 +1,15 @@
 # https://www.youtube.com/watch?v=KOD2BFauQbA
-
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        res = []
-        lst = self.backtrack(n)
+        binary_list = self.backtrack(n)
 
-        for l in lst:
-            decimal_num = int(l, 2) #convert binary to decimal
+        #Convert the binary digits of lst into decimal 
+        res = []
+
+        for bin_list in binary_list:
+            decimal_num = int(bin_list, 2)
             res.append(decimal_num)
+
         return res
 
     def backtrack(self, n):
@@ -18,18 +20,14 @@ class Solution:
         curr = self.backtrack(n - 1)
 
         lst = []
-
+        
         for i in range(len(curr)):
-            print("inside 1st for loop")
             lst.append('0' + curr[i])
 
         for i in range(len(curr) - 1, -1, -1):
             lst.append('1' + curr[i])
-        
+
         return lst
 
 
         
-
-
-                
