fac04b7e0d49be28ec9d45b3b2b4df7733229a27
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-21 14:43:15 +0530

    Time: 454 ms (81.22%), Space: 35.1 MB (71.4%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,26 +1,35 @@
+# TC = O(N * (2 ^ N))
+# SC = O(2 ^ N)       #Because of recursion tree.
+
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
-        self.backtrack(0, [], res, s)
-        return res
+        part = []
+        
+        def dfs(i):
+            if i >= len(s):
+                res.append(part.copy())     #Because 'res' and 'curr' reference to the same array so, and in later code we have to apply operation on curr but not on res so, we have to create DEEP COPY. 
 
-    def backtrack(self, i, part, res, s):
-        if i == len(s):
-            res.append((part.copy()))
-            return 
-
-        for j in range(i, len(s)):
-            if self.is_palindrome(i, j, s):
-                part.append(s[i: j + 1])
-                self.backtrack(j + 1, part, res, s)
-                part.pop()
+                return 
             
-    def is_palindrome(self, l, r, s):
-        while(l < r):
+            for j in range(i, len(s)):
+                #If string index 'i' to 'j' (Both Include) is Palindrome then, we append in list 'part' and after we call 'j + 1'
+                if self.isPalindrome(s, i, j):
+                    part.append(s[i: j + 1])
+                    dfs(j + 1)
+                    part.pop()      #Delete recently add(string), just before 2 lines.
+        
+        dfs(0)
+        return res
+                    
+        #Code to check the given length is Palindrom or not(l and r both are included)    
+    def isPalindrome(self, s, l, r):
+        while l < r:
             if s[l] != s[r]:
                 return False
-
-            l += 1
+                
+            l +=1
             r -= 1
-
+            
         return True
