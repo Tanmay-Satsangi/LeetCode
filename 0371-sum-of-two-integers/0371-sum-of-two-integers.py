cbcad41e3bba0654573fdacd0f51e83deb508bb4
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-22 15:07:13 +0530

    Time: 30 ms (83.32%), Space: 16.5 MB (71.12%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -7,4 +7,5 @@ class Solution:
             a = (a ^ b)
             b = carry
 
-        return (a & mask) if b > 0 else a
+        return (a & mask) if b > 0 else a # because addition of two 32 bit integer may exceed 32 bit integer. So, 
+                            # we have to do mask to make sure that integer does not exceed 32 bit integer
