cd91c8c893801072e137c66ad8fb814b14997820
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-06-27 16:55:59 +0530

    Time: 33 ms (69.23%), Space: 16.4 MB (61.13%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        maxm = 0
+        chunks_count = 0
+        n = len(arr)
+
+        for i in range(n):
+            maxm = max(maxm, arr[i])
+
+            if maxm == i:
+                chunks_count += 1
+
+        return chunks_count
