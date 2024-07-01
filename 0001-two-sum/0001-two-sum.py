420610ec29d6345911d8bd9ce9133382fdf3e1aa
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-07-01 22:32:51 +0530

    Time: 49 ms (95.61%), Space: 17.6 MB (58.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,7 +5,6 @@ class Solution:
         
         for i in range(len(nums)):
             rem = target - nums[i]
-            val = nums[i]
 
             if (rem in dict1) and (dict1[rem] != i):
                 return [i, dict1[rem]]
