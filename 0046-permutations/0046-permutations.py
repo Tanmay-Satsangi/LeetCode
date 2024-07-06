b5283a3cad446dbdbe25dfbf43e94df61b79558e
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-07-07 00:31:14 +0530

    Time: 44 ms (32.48%), Space: 16.6 MB (87.8%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,18 +2,19 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        self.backtrack([], res, nums)
+        self.backtrack([], nums, res) #No need of start because loop always start from '0'.
         return res
 
-    def backtrack(self, curr, res, nums):
+    def backtrack(self, curr, nums, res): #No need o
         if len(curr) == len(nums):
             res.append(list(curr))
-            return
 
         for i in range(len(nums)):
             if nums[i] in curr:
                 continue
 
             curr.append(nums[i])
-            self.backtrack(curr, res, nums)
+            self.backtrack(curr, nums, res)
             curr.pop()
+
+        
