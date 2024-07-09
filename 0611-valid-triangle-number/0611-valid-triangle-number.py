c79c3fb0740d8c2abef389f686829d749e1e3cfb
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-07-09 15:25:11 +0530

    Time: 469 ms (90.61%), Space: 16.5 MB (83.57%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -0,0 +1,31 @@
+# https://www.youtube.com/watch?v=PqEiJDdt3S4
+# TC = O(N2)
+# SC = O(1)
+
+#Property of Traingle: Sum of any two sides should be strictly greater then the third side
+# i.e (a + b) > c
+    
+
+class Solution:
+    def triangleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        nums.sort()        
+        count = 0
+
+        for i in range(n - 1, -1, -1):
+            l = 0
+            r = i - 1
+
+            while (l < r):
+                if (nums[l] + nums[r] > nums[i]): #Here three sides of triangle are arr[l], arr[r] and arr[i]
+                    # (if arr[l] + arr[r] > arr[i] then definitely upcoming greater values of arr[l] is always greater because array is sorted)
+                    count += (r - l)
+                    r -= 1
+                else:
+                    # If sum of arr[l] + arr[r] is smaller then arr[i] then we have to increase the value 
+                    l += 1
+
+        return count
+
+        
