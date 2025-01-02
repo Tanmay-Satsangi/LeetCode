29ca46e9349805bee83d3467403581390431f289
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-02 14:12:49 +0530

    Time: 3 ms (71.93%), Space: 18.4 MB (16.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,11 +5,12 @@ class Solution:
         """
         len_row = len(matrix)
         len_col = len(matrix[0])
-
+        
         first_row_has_zero = False
         first_col_has_zero = False
-
-        # Track zeroes
+        
+        #Find the position of zero ans update zero on first row and first column.
+        #also, update first_row_has_zero and first_col_has_zero.
         for r in range(len_row):
             for c in range(len_col):
                 if matrix[r][c] == 0:
@@ -18,19 +19,18 @@ class Solution:
                     if c == 0:
                         first_col_has_zero = True
                     matrix[r][0] = matrix[0][c] = 0
-
-        # Iterate the rows and column from (1, 1)
+                    
+        #From 1st row and 1st column update zero according to 1st row and 1st column of that value
         for r in range(1, len_row):
             for c in range(1, len_col):
-                if  matrix[r][0] == 0 or matrix[0][c] == 0:
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
-
+         
+        #convert element into zero of first row and first column
         if first_row_has_zero:
             for c in range(len_col):
                 matrix[0][c] = 0
-
+                
         if first_col_has_zero:
             for r in range(len_row):
                 matrix[r][0] = 0
-
-        
