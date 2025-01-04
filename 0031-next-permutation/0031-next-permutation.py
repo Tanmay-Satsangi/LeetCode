297d57821eedb5add63fef62339f4a2f2d0287be
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-04 14:23:31 +0530

    Time: 36 ms (0.28%), Space: 14.3 MB (100%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,21 +4,31 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums) - 1
-
-        while n > 0 and nums[n - 1] >= nums[n]:
-            n -= 1 
         
+        #find the element which have next element is not greater
+        while n > 0 and nums[n - 1] >= nums[n]:
+            n -= 1
+         
+       #if traverse the whole array is descending order. 
         if n == 0:
-            return nums.reverse()
+            nums.reverse()
+            return nums
         
         i = n - 1
-        j = len(nums) - 1
-
+        j = len(nums) - 1  
+        
+        #find the element that is greater than non descending element.
         while nums[i] >= nums[j]:
-                j -= 1
-            
-        nums[i], nums[j] = nums[j], nums[i]
-
-        nums[n:] = nums[n:][::-1]
-
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]    
+        
+        # reverse the array from non - descending element to last element 
+        l, r = n, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+        
+        return nums
+        
         
