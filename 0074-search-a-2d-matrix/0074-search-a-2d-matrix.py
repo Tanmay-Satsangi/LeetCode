e0dc9c885cad7f26943d3566f79c60a65215b39d
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-07 18:32:03 +0530

    Time: 0 ms (100%), Space: 18.2 MB (24.17%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -9,6 +9,9 @@ class Solution:
         while (low <= high):
             mid = (low + high) // 2
 
+            # To convert the 1D array index into 2D array index.
+            # 1st element of each row is always multiple of column
+            # and column element is modulus of column i.e. how much we have to go extra from 1st element of row.
             mid_element = matrix[mid // len_col][mid % len_col]
 
             if mid_element == target:
