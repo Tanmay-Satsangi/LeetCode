987b4147557f3f6283f17e54644c8fbe599abae0
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-17 16:55:15 +0530

    Time: 0 ms (100%), Space: 17.9 MB (13.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,3 +1,9 @@
+#m = Number of rows
+#n = Numbere of columns
+
+# TC = O(N)
+# SC = O(1)
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         N = m + n - 2
@@ -8,3 +14,40 @@ class Solution:
             res = res * (N - r + i) // i
 
         return res
+
+#Explanation of above solution(Striver) and Bosscoder Academy solution. But prefer the Striver solution.
+
+#m = Number of rows
+#n = Numbere of columns
+
+# TC = O(N)
+#SC = O(1)
+
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+        #One Way
+        # N = m + n - 2  #Number of rows + Number of columns - 2
+        # r = m - 1   #Number of rows - 1
+        # res = 1
+        
+        # #Calculate: N
+        # #            C
+        # #             r
+        
+        # for i in range(1, r + 1):
+        #     res = res * (N - r + i) / i
+            
+        # return int(res)
+
+        #Another Way(By Bosscoder Academy i.e. Amit Kumar)
+        # dp = [[0 for _ in range(n)] for _ in range(m)]
+        # for i in range(m):
+        #     for j in range(n):
+        #         if (i == 0 or j == 0):
+        #             dp[i][j] = 1
+
+        # for i in range(1, m):
+        #     for j in range(1, n):
+        #         dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        # return dp[m - 1][n - 1]
