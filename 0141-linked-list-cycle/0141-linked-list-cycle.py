4c53b82f9afd9aee8f23478788aca8605312f874
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-24 15:49:53 +0530

    Time: 50 ms (40.32%), Space: 19.9 MB (42.6%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -15,7 +15,7 @@ class Solution:
 
         fast = slow = head
 
-        while fast and fast.next:
+        while fast.next and fast.next.next:
             fast = fast.next.next
             slow = slow.next
 
