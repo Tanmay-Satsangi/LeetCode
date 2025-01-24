863a661d5b2dc94a73f951e1dfb38c80e2596c4a
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-24 16:45:16 +0530

    Time: 23 ms (83.95%), Space: 39.5 MB (23.07%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,3 +1,8 @@
+# TC = O(N/2) + O(N/2)
+# N/2 -> to find middle of linked list
+# N/2 -> to reverse the half linked list(slow.next to last)
+# SC = O(1)
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,38 +10,40 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if head == None or head.next == None:
+        if not head or not head.next:
             return True
-
-        slow = fast = head
-
-        # Find half of the linked list
+        
+        #Find middle of linked list
+        slow = head
+        fast = head
+        
         while fast.next and fast.next.next:
-            fast = fast.next.next
             slow = slow.next
-
-        slow.next = self.reverse_linked_list(slow.next)
-
+            fast = fast.next.next
+        
+        #Reverse the half Linked list and slow.next point to the half linked list
+        slow.next = self.reverseLinkedList(slow.next)
+        
+        #Move slow to right half
         slow = slow.next
-
+        
         while slow:
             if head.val != slow.val:
                 return False
-
             head = head.next
             slow = slow.next
-
         return True
-
     
-    def reverse_linked_list(self, head):
+    #Function to reverse the half linked list.
+    def reverseLinkedList(self, head):
         prev = None
         curr = head
-
+        
         while curr:
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
-
+            
         return prev
+        
