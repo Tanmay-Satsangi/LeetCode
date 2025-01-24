69951730db48bb1fdce54dbfc14149f4fa6b24e6
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-24 15:22:42 +0530

    Time: 86 ms (46.21%), Space: 27.8 MB (61.1%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -9,19 +9,11 @@ class Solution:
         d1 = headA
         d2 = headB
 
-        # When d1 and d2 are not null
         while d1 != None or d2 != None:
             if d1 == d2:
                 return d1
-
-            if d1 != None:
-                d1 = d1.next
-            else:
-                d1 = headB
-
-            if d2 != None:
-                d2 = d2.next
-            else:
-                d2 = headA
+                
+            d1 = d1.next if d1 else headB
+            d2 = d2.next if d2 else headA
 
         return None
