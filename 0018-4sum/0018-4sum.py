4c181d90af06bb2ec85ab929d228f67530356b27
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-25 19:23:57 +0530

    Time: 647 ms (15.15%), Space: 17.9 MB (39.45%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,45 +1,53 @@
+# TC = O(Nlogn) + O(N ^ 3) 
+# therefore, TC = O(N ^ 3)
+# SC = O(1)
+
+# APPROACH: 2 loop + 2 pointer
+
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
-
+        
         if n < 4:
             return []
-
+        
         nums.sort()
-        i = 0
         ans = []
-
-        while (i < n):
+        i = 0
+        while i < n:
             j = i + 1
-
-            while (j < n):
+            #rem = target - nums[i]  cannot use in 4 sum try ex: [1, 0, -1, -2, 2] and target = 0
+            while j < n:
                 left = j + 1
                 right = n - 1
-
+                # rem = rem - nums[j] cannot use in 4 sum try ex: [1, 0, -1, -2, 2] and target = 0 because 2nd time this j loop runs we have to again update rem = target - nums[i]
+                
                 while left < right:
-                    sums = nums[i] + nums[j] + nums[left] + nums[right]
-                    if sums == target:
+                    if nums[i] + nums[j] + nums[left] + nums[right] == target:
                         ans.append([nums[i], nums[j], nums[left], nums[right]])
                         left += 1
                         right -= 1
-
-                        while (left < n and nums[left] == nums[left - 1]):
+                        
+                        #increment left(use loop to avoid duplicate values)
+                        while left < n and nums[left] == nums[left - 1]:
                             left += 1
-
-                        while (right >= 0 and nums[right] == nums[right + 1]):
+                        #increment right(use loop to avoid duplicate values)
+                        while right >= 0 and nums[right] == nums[right + 1]:
                             right -= 1
-
-                    elif sums < target:
+                        
+                    elif nums[i] + nums[j] + nums[left] + nums[right] < target:
                         left += 1
                     else:
                         right -= 1
-
+                    
+                #increment j
                 j += 1
-                while (j < n and nums[j] == nums[j - 1]):
+                while j < n and nums[j] == nums[j - 1]:
                     j += 1
-
+                    
+            #increment i
             i += 1
-            while (i < n and nums[i] == nums[i - 1]):
+            while i < n and nums[i] == nums[i - 1]:
                 i += 1
-
+            
         return ans
