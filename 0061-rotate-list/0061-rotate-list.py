913782dde0238efc48c1aca11745b0db488ec79d
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-01-26 18:56:26 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.54%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,15 +1,3 @@
-#TC = O(N) + O(N - N % K) 
-#TC = O(N)
-#SC = O(1)
-
-#Steps to solve this question
-# 1. Count length of the linked list
-# 2. next pointer of last node will pointed to head(Create Circular linked list)
-# 3. k = len - k
-# 4. k = n % k
-# 5. next of kth node to None.
-
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -19,27 +7,26 @@ class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        
-        #Count number of nodes in Linked list
+
         curr = head
-        n = 1   #Length of the linked list
+        n = 1
+
         while curr.next:
             n += 1
             curr = curr.next
-        
-        #Create circular singly linked list
-        curr.next = head
-        
+
+        curr.next = head 
+
         k = k % n
-        k = n - k
-        
+        k = n - k - 1
+
         newHead = head
-        while k != 1:
+
+        while k:
             newHead = newHead.next
             k -= 1
-         
+
         head = newHead.next
         newHead.next = None
-        
-        
+
         return head
