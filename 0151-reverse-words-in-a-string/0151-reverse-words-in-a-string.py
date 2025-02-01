ccfe872d64fea171d85abf79bad6e26a1587a9df
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-01 21:39:58 +0530

    Time: 0 ms (100%), Space: 17.8 MB (55.44%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -0,0 +1,14 @@
+# https://www.youtube.com/watch?v=VcBy0s5q2kk
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.strip().split(" ") #  We have to use the extra space because in Python string are immutable so, we cannot modifty the string.
+
+        n = len(words)
+        ans = []
+        
+        for i in range(n - 1, -1, -1):
+            if len(words[i]) > 0:
+                ans.append(words[i])
+
+        return " ".join(ans)
