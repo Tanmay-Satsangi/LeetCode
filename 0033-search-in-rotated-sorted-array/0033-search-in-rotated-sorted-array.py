5582d6d43746046f7b69df8f9ba6a724e02a2a31
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-01 16:36:37 +0530

    Time: 0 ms (100%), Space: 18.2 MB (17.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,26 +1,29 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+
         n = len(nums)
-
+        
         low = 0
-        high = n - 1
+        high = (n - 1)
 
-        while (low <= high):
+        while(low <= high):
             mid = (low + high) // 2
 
             if nums[mid] == target:
                 return mid
 
-            #Either low to mid is always sorted or mid to high part is always sorted.
-            # = is required when the element is not present and low, high and mid pointed to same index.
-            if nums[low] <= nums[mid]:  
-                if nums[low] <= target < nums[mid]:
+            #Check 1st part is sorted
+            #Remember there is equal to sign TEST CASE: [3, 1] and target = 1
+            if nums[low] <= nums[mid]:
+                #check target is present in 1st part
+                if nums[low] <= target and target < nums[mid]:
                     high = mid - 1
                 else:
                     low = mid + 1
 
+            #If 1st part is not sorted then definetely 2nd part is sorted.
             else:
-                if nums[mid] < target <= nums[high]:
+                if nums[mid] < target and target <= nums[high]:
                     low = mid + 1
                 else:
                     high = mid - 1
