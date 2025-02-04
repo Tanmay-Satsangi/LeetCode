965c16816dad3b18e2e29703c47f18acfc15804c
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-04 12:42:06 +0530

    Time: 19 ms (17.04%), Space: 18.2 MB (20.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,5 @@
+# 1st Approach: sort both the string lexicographically by using sorted function and compare the sorted string. if both sorted string are same then return True else False
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dicts = {}
-        dictt = {}
-
-        for i in range(len(s)):
-            if s[i] not in dicts:
-                dicts[s[i]] = 1
-            else: 
-                dicts[s[i]] += 1
-        
-        for i in range(len(t)):
-            if t[i] not in dictt:
-                dictt[t[i]] = 1
-            else: 
-                dictt[t[i]] += 1
-
-        return dicts == dictt
-        
+        return ''.join(sorted(s)) == ''.join(sorted(t))
