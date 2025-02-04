c04b2de364b815a7397535486c6769fe85464115
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-04 13:09:11 +0530

    Time: 7 ms (90.18%), Space: 17.9 MB (57.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,14 @@
-# 1st Approach: sort both the string lexicographically by using sorted function and compare the sorted string. if both sorted string are same then return True else False
+# 1st Approach: Sort both strings lexicographically using sorted(), then compare them. If they match, return True; otherwise, return False.
 
+# TC = O(N logN)
+# SC = O(N) # For list of characters
+
+# return ''.join(sorted(s)) == ''.join(sorted(t))  # sorted() returns a list of characters.
+
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return ''.join(sorted(s)) == ''.join(sorted(t))
+        s_dict = Counter(s)
+        t_dict = Counter(t)
+
+        return s_dict == t_dict
