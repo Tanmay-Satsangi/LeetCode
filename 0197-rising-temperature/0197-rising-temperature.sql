c4c53c579e397e89c610ca0218858a172ffead50
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-07 15:42:56 +0530

    Time: 803 ms (52%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,5 +3,7 @@
 -- self join 
 
 select w2.id 
-from Weather w1, Weather w2
-where DateDiff(w2.recordDate, w1.recordDate) = 1 and w2.temperature > w1.temperature
+from Weather w1
+join Weather w2
+on DateDiff(w2.recordDate, w1.recordDate) = 1 
+where w2.temperature > w1.temperature
