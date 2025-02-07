156a6ce76e2f33bdd4f6d9db0a5914beea1e7fa5
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-07 18:47:44 +0530

    Time: 1508 ms (36.78%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -2,6 +2,14 @@
 
 -- Use IFNULL() to convert NULL values to 0.
 
+-- Still I did not understand it. 
+
+-- Tricky question if I used where at line 11 then it gives different output.
+-- Why They Yield Different Outputs
+-- Query 1 returns all products (even those with no sales during the date range) with an average price of 0 if there are no matching sales.
+-- Query 2 returns only products with sales in the specified date range, excluding those with no sales, which leads to a different output set.
+-- Understanding when the filter is applied (during the join vs. after the join) is key to grasping the difference between these two queries.
+
 select p.product_id, IFNULL(round(sum(u.units * p.price) / sum(u.units), 2), 0) as average_price
 from Prices p 
 left join UnitsSold u
