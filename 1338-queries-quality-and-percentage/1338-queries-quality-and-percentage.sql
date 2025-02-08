7639d7ce2697e2c42ceafadcdd7446cf6f80cbbf
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-08 18:12:34 +0530

    Time: 643 ms (50.01%), Space: 0B (100%) - LeetHub

---
+++ 1338-queries-quality-and-percentage/1338-queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select query_name, ROUND(AVG(rating / position), 2) as quality, 
+ROUND(AVG(IF(rating < 3, 1, 0)) * 100, 2) AS poor_query_percentage
+from Queries
+group by query_name
