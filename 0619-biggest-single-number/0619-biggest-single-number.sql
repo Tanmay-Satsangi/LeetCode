e8619bb1672a01f98d58ee2ae2943fb8fc75fb13
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-02-08 10:39:29 +0530

    Time: 923 ms (19.29%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -2,6 +2,8 @@
 
 -- here aliasing is compulsory otherwise it gives the error: "every derived table must have its own alias"
 
+-- Reason of Error: This error occurs when you use a subquery (derived table) inside a FROM clause without assigning it an alias.
+
 -- https://stackoverflow.com/questions/1888779/what-is-the-error-every-derived-table-must-have-its-own-alias-in-mysql
 
 select max(num) as num from (select num
