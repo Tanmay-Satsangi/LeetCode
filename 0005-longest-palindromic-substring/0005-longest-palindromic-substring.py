25c5a60a71c038d5e1bb122db89c653d9eb30744
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-05-22 12:02:58 +0530

    Time: 255 ms (85.01%), Space: 18 MB (28.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,7 +4,6 @@ class Solution:
         n = len(s)
 
         for i in range(n):
-            # For Odd String
             pal_start1, pal_len1 = self.expand_around_centers(s, i, i)
             pal_start2, pal_len2 = self.expand_around_centers(s, i, i + 1)
 
@@ -15,8 +14,8 @@ class Solution:
                 pal_max_len, pal_start = pal_len2, pal_start2
 
         return s[pal_start: pal_start + pal_max_len]
-
         
+
     def expand_around_centers(self, s, l, r):
         while (l >= 0 and r < len(s) and s[l] == s[r]):
             l -= 1
