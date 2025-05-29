885b54bb441ee06c7341f9b4ec4f146c1092ba12
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2025-05-29 09:56:49 +0530

    Time: 247 ms (90.8%), Space: 17.9 MB (46.1%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        pal_start = pal_max_len = 0
         n = len(s)
-        pal_max_len = pal_start = 0
 
         for i in range(n):
-            pal_start1, pal_len1 = self.expand_around_centers(s, i, i)
+            pal_start1, pal_len1 =  self.expand_around_centers(s, i, i)
             pal_start2, pal_len2 = self.expand_around_centers(s, i, i + 1)
 
-            if pal_len1 > pal_max_len:
-                pal_max_len, pal_start = pal_len1, pal_start1
-            if pal_len2 > pal_max_len:
-                pal_max_len, pal_start = pal_len2, pal_start2
+            if pal_max_len < pal_len1:
+                pal_start, pal_max_len = pal_start1, pal_len1
+            if pal_max_len < pal_len2:
+                pal_start, pal_max_len = pal_start2, pal_len2
 
-        return s[pal_start: pal_start + pal_max_len]
+        return s[pal_start:pal_start + pal_max_len]
 
     def expand_around_centers(self, s, l, r):
         while (l >= 0 and r < len(s) and s[l] == s[r]):
             l -= 1
             r += 1
 
-        return (l + 1, r - l - 1)
-        
+        return l + 1, r - l - 1
